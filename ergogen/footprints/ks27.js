8f05802fbb8f8359458f082fbfe27f835eaf6a9d
theb0b12 <103223932+theb0b12@users.noreply.github.com>
2026-09-03 21:55:45 -0600

    updated pcbs and ergogen

--- ergogen/footprints/ks27.js
+++ ergogen/footprints/ks27.js
@@ -29,36 +29,15 @@ fp.push(`(pad "" np_thru_hole circle (at 4.4 ${flipN(flip, 4.7)} ${flipR(flip, p
 fp.push(`(pad "" np_thru_hole circle (at 0 ${flipN(flip, 0)} ${flipR(flip, p.r + 0)}) (size 5 5) (drill 5) (layers "F&B.Cu" "*.Mask") )`);
 fp.push(`(pad "" np_thru_hole circle (at 2.6 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 3 3) (drill 3) (layers "F&B.Cu" "*.Mask") )`);
 
-fp.push(`(pad "1" smd rect (at -7.675 ${flipN(flip, 4.7)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.from})`);
+fp.push(`(pad "1" smd rect (at -7.675 ${flipN(flip, 4.7)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.to})`);
 // fp.push(`(pad "1" smd rect (at -5.875 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.from})`);
-fp.push(`(pad "2" smd rect (at 5.875 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.to})`);
+fp.push(`(pad "2" smd rect (at 5.875 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.from})`);
 // fp.push(`(pad "2" smd rect (at 7.675 ${flipN(flip, 4.7)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(flip ? "F" : "B")}.Cu" "${(flip ? "F" : "B")}.Paste" "${(flip ? "F" : "B")}.Mask")  ${p.to})`);
 
 // fp.push(`(pad "1" smd rect (at -7.675 ${flipN(flip, 4.7)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(!flip ? "F" : "B")}.Cu" "${(!flip ? "F" : "B")}.Paste" "${(!flip ? "F" : "B")}.Mask")  ${p.from})`);
 fp.push(`(pad "1" smd rect (at -5.875 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(!flip ? "F" : "B")}.Cu" "${(!flip ? "F" : "B")}.Paste" "${(!flip ? "F" : "B")}.Mask")  ${p.from})`);
 // fp.push(`(pad "2" smd rect (at 5.875 ${flipN(flip, 5.75)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(!flip ? "F" : "B")}.Cu" "${(!flip ? "F" : "B")}.Paste" "${(!flip ? "F" : "B")}.Mask")  ${p.to})`);
 fp.push(`(pad "2" smd rect (at 7.675 ${flipN(flip, 4.7)} ${flipR(flip, p.r + 0)}) (size 2.6 2.6) (layers "${(!flip ? "F" : "B")}.Cu" "${(!flip ? "F" : "B")}.Paste" "${(!flip ? "F" : "B")}.Mask")  ${p.to})`);
-
-
-// Drawings on F.Fab
-// fp.push(`(fp_text value "gateron-ks27-hotswap-reversible" (at 0 ${flipN(flip, 8.5)} ${flipR(flip, p.r) % 180}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (effects (font (size 1 1) (thickness 0.15)) (justify${ flip ? " mirror" : ""})) )`);
-// fp.push(`(fp_line (start 7.5 ${flipN(flip, 7.5)}) (end 7.5 ${flipN(flip, -7.5)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.1) )`);
-// fp.push(`(fp_line (start -7.5 ${flipN(flip, -7.5)}) (end -7.5 ${flipN(flip, 7.5)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.1) )`);
-// fp.push(`(fp_line (start -7.5 ${flipN(flip, 7.5)}) (end 7.5 ${flipN(flip, 7.5)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.1) )`);
-// fp.push(`(fp_line (start 7.5 ${flipN(flip, -7.5)}) (end -7.5 ${flipN(flip, -7.5)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.1) )`);
-
-// // Drawings on F.SilkS
-// fp.push(`(fp_text reference "REF**" (at 0.1 ${flipN(flip, -8.5)} ${flipR(flip, p.r ) % 180}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (effects (font (size 1 1) (thickness 0.15)) (justify${ flip ? " mirror" : ""})) )`);
-
-// // Drawings on Eco2.User
-// fp.push(`(fp_line (start 1.8 ${flipN(flip, -6.3)}) (end -3.2 ${flipN(flip, -6.3)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start -7 ${flipN(flip, -7)}) (end -7 ${flipN(flip, 7)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start 7 ${flipN(flip, 7)}) (end 7 ${flipN(flip, -7)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start -3.2 ${flipN(flip, -6.3)}) (end -3.2 ${flipN(flip, -3.1)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start 7 ${flipN(flip, -7)}) (end -7 ${flipN(flip, -7)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start -7 ${flipN(flip, 7)}) (end 7 ${flipN(flip, 7)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start 1.8 ${flipN(flip, -6.3)}) (end 1.8 ${flipN(flip, -3.1)}) (layer "Eco2.User") (width 0.12) )`);
-// fp.push(`(fp_line (start 1.8 ${flipN(flip, -3.1)}) (end -3.2 ${flipN(flip, -3.1)}) (layer "Eco2.User") (width 0.12) )`);
 
     fp.push(')');
     return fp.join('\n');
@@ -72,4 +51,3 @@ function normalizeAngle(angle) {
 }
 function flipR(flip, r) { return normalizeAngle(flip ? (180 - r) : r) }
 function flipN(flip, n) { return flip ? -n : n }
-
--- ergogen/footprints/usbc.js
+++ ergogen/footprints/usbc.js
@@ -43,21 +43,21 @@ fp.push(`(pad "A9" smd rect (at 1.52 ${flipN(flip, -4.09)} ${flipR(flip, p.r + 0
 fp.push(`(pad "A5" smd rect (at -0.5 ${flipN(flip, -4.09)} ${flipR(flip, p.r + 0)}) (size 0.7 1.4) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (solder_mask_margin 0.102) ${p.A5})`);
 fp.push(`(pad "B5" smd rect (at 0.5 ${flipN(flip, -4.09)} ${flipR(flip, p.r + 0)}) (size 0.7 1.4) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (solder_mask_margin 0.102) ${p.B5})`);
 
-// Drawings on F.CrtYd
-fp.push(`(fp_line (start -5.07 ${flipN(flip, -5.04)}) (end 5.07 ${flipN(flip, -5.04)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
-fp.push(`(fp_line (start 5.07 ${flipN(flip, -5.04)}) (end 5.07 ${flipN(flip, 2.85)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
-fp.push(`(fp_line (start 5.07 ${flipN(flip, 2.85)}) (end -5.07 ${flipN(flip, 2.85)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
-fp.push(`(fp_line (start -5.07 ${flipN(flip, 2.85)}) (end -5.07 ${flipN(flip, -5.04)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
+// // Drawings on F.CrtYd
+// fp.push(`(fp_line (start -5.07 ${flipN(flip, -5.04)}) (end 5.07 ${flipN(flip, -5.04)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
+// fp.push(`(fp_line (start 5.07 ${flipN(flip, -5.04)}) (end 5.07 ${flipN(flip, 2.85)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
+// fp.push(`(fp_line (start 5.07 ${flipN(flip, 2.85)}) (end -5.07 ${flipN(flip, 2.85)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
+// fp.push(`(fp_line (start -5.07 ${flipN(flip, 2.85)}) (end -5.07 ${flipN(flip, -5.04)}) (layer "${(flip ? "B.CrtYd" : "F.CrtYd")}") (width 0.05))`);
 
-// Drawings on F.Fab
-fp.push(`(fp_text value CUI_UJC-HP2-3-SMT-TR (at 8.2 ${flipN(flip, -5.885)} ${flipR(flip, p.r + 0) % 180}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (effects (font (size 1.0 1.0) (thickness 0.15)) (justify${ flip ? " mirror" : ""})))`);
-fp.push(`(fp_line (start -4.47 ${flipN(flip, -4.2)}) (end -4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
-fp.push(`(fp_line (start -4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
-fp.push(`(fp_line (start 4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, -4.2)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
-fp.push(`(fp_line (start 4.47 ${flipN(flip, -4.2)}) (end -4.47 ${flipN(flip, -4.2)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
+// // Drawings on F.Fab
+// fp.push(`(fp_text value CUI_UJC-HP2-3-SMT-TR (at 8.2 ${flipN(flip, -5.885)} ${flipR(flip, p.r + 0) % 180}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (effects (font (size 1.0 1.0) (thickness 0.15)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(fp_line (start -4.47 ${flipN(flip, -4.2)}) (end -4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
+// fp.push(`(fp_line (start -4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
+// fp.push(`(fp_line (start 4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, -4.2)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
+// fp.push(`(fp_line (start 4.47 ${flipN(flip, -4.2)}) (end -4.47 ${flipN(flip, -4.2)}) (layer "${(flip ? "B.Fab" : "F.Fab")}") (width 0.127))`);
 
 // Drawings on F.SilkS
-fp.push(`(fp_text reference REF** (at -1.325 ${flipN(flip, -7.385)} ${flipR(flip, p.r + 0) % 180}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (effects (font (size 1.0 1.0) (thickness 0.15)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(fp_text reference REF** (at -1.325 ${flipN(flip, -7.385)} ${flipR(flip, p.r + 0) % 180}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (effects (font (size 1.0 1.0) (thickness 0.15)) (justify${ flip ? " mirror" : ""})))`);
 fp.push(`(fp_line (start -4.47 ${flipN(flip, 1.12)}) (end -4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (width 0.127))`);
 fp.push(`(fp_line (start -4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, 2.6)}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (width 0.127))`);
 fp.push(`(fp_line (start 4.47 ${flipN(flip, 2.6)}) (end 4.47 ${flipN(flip, 1.12)}) (layer "${(flip ? "B.SilkS" : "F.SilkS")}") (width 0.127))`);
@@ -76,4 +76,3 @@ function normalizeAngle(angle) {
 }
 function flipR(flip, r) { return normalizeAngle(flip ? (180 - r) : r) }
 function flipN(flip, n) { return flip ? -n : n }
-
